2021091215772659a640585e5d43a7dc5576ec2b
tpmacc <tpmacc@yahoo.com>
2022-01-16 01:59:31 -0500

    Make project deployable on Okteto

--- src/main/java/skeleton/greeting/GreetingController.groovy
+++ src/main/java/skeleton/greeting/GreetingController.groovy
@@ -1,6 +1,6 @@
 package skeleton.greeting
 
-
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -10,13 +10,26 @@ import java.util.concurrent.atomic.AtomicLong
 @RestController
 class GreetingController {
 
+    @Value('${skel.envvar}')
+//    @Value("${systemValue}")
+    String variable;
+
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping("/param")
+        Greeting greetingWithRequestParam(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(
                 id: counter.incrementAndGet(),
                 content: "Hello, ${name}"
         )
     }
+
+    @RequestMapping("/variable")
+    Greeting greetingWithEnvironmentVariable() {
+        return new Greeting(
+                id: counter.incrementAndGet(),
+                content: "Hello, ${variable}"
+        )
+    }
+
 }
